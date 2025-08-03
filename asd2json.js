203259052bb8bbb9252c96a1a9e6c20029be4a74
jml <jml@WINNER>
2025-08-03 16:53:24 +0200

    quote

--- asd2json.js
+++ asd2json.js
@@ -11,7 +11,7 @@ case "-h":
 }
 let o = 0;
 let d = "{";
-d += "I:[" ;
+d += "\"I\":[" ;
 let n = (h[2] + (h[3]<<8) + (h[4]<<16) + (h[5]<<24));
 d += n;
 o = 6;
@@ -21,21 +21,23 @@ d += "," + (h[o] + (h[o+1]<<8) + (h[o+2]<<16) + (h[o+3]<<24));
 d += "]\n";
 o = 48 + n * 4;
 n = (h[o] + (h[o+1]<<8) + (h[o+2]<<16) + (h[o+3]<<24));
-d += ",SampleData:" + n;
+d += ",\"SampleData\":" + n;
 o += 16;
-d += ",SampleData$:" + (h[o] + (h[o+1]<<8) + (h[o+2]<<16) + (h[o+3]<<24));
+d += ",\"SampleData$\":" + (h[o] + (h[o+1]<<8) + (h[o+2]<<16) + (h[o+3]<<24));
 o += 4;
 n = (h[o] + (h[o+1]<<8) + (h[o+2]<<16) + (h[o+3]<<24));
 console.log("o = " + o + " " + n);
 let i;
 o += 4;
+d += ", \"";
 for (i = 0; i < n; i += 1) {
 
 	d += String.fromCodePoint(h[o+i*2]);
 }
+d += "\":0";
 
 
-d += "}\n";
+d += "\n}\n";
 fs.writeFileSync(process.argv[4], d, "utf8");
 fs.appendFileSync(process.argv[4], "", "utf8");
 
