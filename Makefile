135ff8ca8ced73277a246b7badb9968a0b71d0a1
jml <jml@WINNER>
2025-08-03 16:47:05 +0200

    asd

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@ all:
 	node toamxd.js -m source/m.json  devices/m.amxd
 	node toamxd.js -a source/a.json  devices/a.amxd
 	node toamxd.js -i source/i.json  devices/i.amxd
-
+	node asd2json.js -h media/untitled2.wav.asd media/u.json
+	type media\u.json
+	node asd2json.js -h media/m.wav.asd media/mu.json
+	type media\mu.json
 
---
+++ asd2json.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+console.log("hello");
+
+let f = fs.readFileSync(process.argv[3]);
+let h = new Uint8Array(f);
+
+switch (process.argv[2]) {
+case "-h":
+	break;
+}
+let o = 0;
+let d = "{";
+d += "I:[" ;
+let n = (h[2] + (h[3]<<8) + (h[4]<<16) + (h[5]<<24));
+d += n;
+o = 6;
+d += "," + (h[o] + (h[o+1]<<8) + (h[o+2]<<16) + (h[o+3]<<24));
+o = 10;
+d += "," + (h[o] + (h[o+1]<<8) + (h[o+2]<<16) + (h[o+3]<<24));
+d += "]\n";
+o = 48 + n * 4;
+n = (h[o] + (h[o+1]<<8) + (h[o+2]<<16) + (h[o+3]<<24));
+d += ",SampleData:" + n;
+o += 16;
+d += ",SampleData$:" + (h[o] + (h[o+1]<<8) + (h[o+2]<<16) + (h[o+3]<<24));
+o += 4;
+n = (h[o] + (h[o+1]<<8) + (h[o+2]<<16) + (h[o+3]<<24));
+console.log("o = " + o + " " + n);
+let i;
+o += 4;
+for (i = 0; i < n; i += 1) {
+
+	d += String.fromCodePoint(h[o+i*2]);
+}
+
+
+d += "}\n";
+fs.writeFileSync(process.argv[4], d, "utf8");
+fs.appendFileSync(process.argv[4], "", "utf8");
+
